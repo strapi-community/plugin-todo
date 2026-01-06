eff0dc469078f08f99db646cdcc8a9725cf4cf36
Boaz Poolman <boaz_skater@hotmail.com>
2026-01-06 22:19:56 +0100

    chore: cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,12 @@
 /**
- * Application methods
- */
-import bootstrap from './bootstrap';
-import destroy from './destroy';
-import register from './register';
-
-/**
  * Plugin server methods
  */
-import config from './config';
 import controllers from './controllers';
 import routes from './routes';
 import services from './services';
 import contentTypes from './content-types';
 
 export default {
-  register,
-  bootstrap,
-  destroy,
-  config,
   controllers,
   routes,
   services,
